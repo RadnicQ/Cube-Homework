698b4ba70cbc49a9bb54c08cd4f26e4eb05f080b
RadnicQ <laperdol@gmail.com>
2025-12-06 06:22:58 +0200

    New Homework

--- Assets/Scripts/Exploder.cs
+++ Assets/Scripts/Exploder.cs
@@ -1,14 +1,25 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Exploder : MonoBehaviour
 {
+    private const float s_standartValueFactor = 1f;
+
     [SerializeField] private float _explosionRadius;
     [SerializeField] private float _explosionForce;
-    
-    public void AddForce(List<Rigidbody> cubes, RaycastHit hit)
+
+    public void AddForce(List<Rigidbody> cubes, RaycastHit hit, float valueFactor = s_standartValueFactor)
     {
         foreach (Rigidbody cube in cubes)
-            cube.AddExplosionForce(_explosionForce, hit.transform.position, _explosionRadius);
+            cube.AddExplosionForce(_explosionForce / valueFactor, hit.transform.position, _explosionRadius / valueFactor);
+    }
+
+    public void AddForce(RaycastHit hit)
+    {
+        float valueFactor = hit.transform.localScale.x;
+        List<Rigidbody> cubes = Physics.OverlapSphere(hit.transform.position, _explosionRadius / valueFactor, hit.collider.layerOverridePriority).Select(collider => collider.attachedRigidbody).Where(rigitBody => rigitBody != null).ToList();
+        AddForce(cubes, hit, valueFactor);
     }
 }
--- Assets/Scripts/SpawnInspector.cs
+++ Assets/Scripts/SpawnInspector.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Spawner), typeof(Exploder))]
@@ -34,6 +35,10 @@ public class SpawnInspector : MonoBehaviour
             {
                 List<Rigidbody> spawners = _spawner.GetSpawnablesRaycastHitObject(clonable);
                 _exploder.AddForce(spawners, hit);
+            }
+            else
+            {
+                _exploder.AddForce(hit);
             }
 
             Destroy(hit.collider.gameObject);
